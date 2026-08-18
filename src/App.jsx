efe909edeeb4e7dc35c8171f5b46f895cc33a0d4
ClaudeG08 <gclaude08140@gmail.com>
2026-08-18 19:32:34 +0200

    modifs

--- src/App.jsx
+++ src/App.jsx
@@ -510,7 +510,7 @@ export default function App() {
 
             alt="GILMEAL Logo"
 
-            className="h-12 w-auto object-contain"
+            className="h-16 w-auto object-contain"
 
             onError={(e) => { e.target.style.display = 'none'; }}
 
